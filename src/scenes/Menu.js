92136c0afda598487f2e09f8907b935ccdd03013
Vincent Cheng <viwcheng@ucsc.edu>
2020-04-08 15:47:14 -0700

    Scenes & assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,4 +4,13 @@ class Menu extends Phaser.Scene
     {
         super("menuScene");
     }
+    create()
+    {
+        console.log(this);
+        //display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        
+        //launch the next scene
+        this.scene.start("playScene");
+    }
 }
