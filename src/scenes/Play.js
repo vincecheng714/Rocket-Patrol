dce8df09174c27ba882cef59014d8e8dbba08198
Vincent Cheng <viwcheng@ucsc.edu>
2020-04-10 15:51:06 -0700

    Added rocket and spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,11 +23,30 @@ class Play extends Phaser.Scene
         this.add.rectangle(603, 5, 32, 455, 0xFACADE).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
+        // add spaceship (x3)
+        this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width,260, 'spaceship', 0, 10).setOrigin(0, 0);
+        // define keyboard keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update()
     {
         // scroll starfield
         this.starfield.tilePositionX -= 4;
+
+        // update rocket
+        this.p1Rocket.update();
+
+        // update spaceships
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
     }
 }
